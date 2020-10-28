cdac1b714fe7d080d03baea893d348d1c189343c
Ishraq <ishraqmahid@gmail.com>
2020-10-28 17:45:21 -0400

    forgot to set instance variables as private; did so

--- BankAccount.java
+++ BankAccount.java
@@ -1,7 +1,7 @@
 public class BankAccount{
-	double balance;
-	int accountID;
-	String password;
+	private double balance;
+	private int accountID;
+	private String password;
 	
 	public BankAccount(int a, String p){
 		balance = 0;
