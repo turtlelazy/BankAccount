a7f96eea97bff94aa6993ccbc81c6a03ae22e6ba
Ishraq <ishraqmahid@gmail.com>
2020-10-28 17:09:09 -0400

    Created constructor and instance variables along with the file

---
+++ BankAccount.java
@@ -0,0 +1,15 @@
+public class BankAccount{
+	double balance;
+	int accountID;
+	String password;
+	
+	public BankAccount(int a, String p){
+		balance = 0;
+		accountID = a;
+		password = p;
+		
+	}
+	
+	
+	
+}
